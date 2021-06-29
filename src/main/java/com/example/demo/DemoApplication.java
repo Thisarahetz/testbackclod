4272fdfec767a759dba00f8e4a9db715eabc14f4
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2021-06-29 16:54:13 +0530

    upload project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+	@RestController
+	public class HelloContoler {
+		@GetMapping(path="/hello")
+		public String Hellowoled(){
+			return "helooooooooooooooooooo";
+		}
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
